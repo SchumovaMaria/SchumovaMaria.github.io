54d8b22bd4c4ac9c39181cbd003627c52f53b9ec
Schumova Maria <schumova.maria@gmail.com>
2022-10-25 23:30:07 +0200

    Start day-to-day report

---
+++ main.js
@@ -0,0 +1,10 @@
+const dayInfo = {
+	currentData: '25 Oct 2022',
+	currentlyBased: 'Torrenostra, Torreblanca, Spain',
+	dayPlan: '',
+	dayReport: 'I decided to start some kind of a diary. I would like to fix day-to-day things there that are important or just interesting for me. Maybe something about my progress in front-end development, books I read, interesting topics from podcasts and articles, successes and failures in a day plan implementation and so on. And yes, I think it\'s a good moment to start making some day plans and schedules. Who knows, maybe it would help me to become more organised, disciplined and see better results.',
+	frontend: '',
+	languages: '',
+	sport: '',
+	book: '',
+};
